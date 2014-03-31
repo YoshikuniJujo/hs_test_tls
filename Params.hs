aff24357b52760b298088624820f82bd1d949bcf
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-03-31 05:11:04 +0000

    not use warp-tls

---
+++ Params.hs
@@ -0,0 +1,55 @@
+module Params (getParams) where
+
+import Control.Applicative
+import Data.Either
+import Data.PEM
+import Data.Certificate.X509
+import Data.Certificate.KeyRSA
+import Network.TLS
+import Network.TLS.Extra
+
+import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString as B
+
+getParams :: FilePath -> FilePath -> IO Params
+getParams cf kf = do
+	certs <- readCertificates cf
+	pk <- readPrivateKey kf
+	return $ mkParams certs pk
+
+mkParams :: [X509] -> PrivateKey -> Params
+mkParams certs pk =
+	updateServerParams (\sp -> sp { serverWantClientCert = False }) $
+		defaultParamsServer {
+			pAllowedVersions = [SSL3, TLS10, TLS11, TLS12],
+			pCiphers = ciphers,
+			pCertificates = zip certs $ (Just pk) : repeat Nothing
+		 }
+
+readCertificates :: FilePath -> IO [X509]
+readCertificates filepath = do
+	rights . parseCerts . pemParseBS <$> B.readFile filepath
+
+parseCerts :: Either String [PEM] -> [Either String X509]
+parseCerts (Right pems) =
+	map (decodeCertificate . L.fromChunks . (: []) . pemContent) $ filter
+		(flip elem ["CERTIFICATE", "TRUSTED CERTIFICATE"] . pemName) pems
+parseCerts (Left err) = error $ "cannot parse PEM file: " ++ err
+
+readPrivateKey :: FilePath -> IO PrivateKey
+readPrivateKey filepath = head <$>
+	rights . parseKey . pemParseBS <$> B.readFile filepath
+
+parseKey :: Either String [PEM] -> [Either String PrivateKey]
+parseKey (Right pems) =
+	map (fmap (PrivRSA . snd) . decodePrivate . L.fromChunks . (: []) . pemContent) $
+		filter ((== "RSA PRIVATE KEY") . pemName) pems
+parseKey (Left err) = error $ "Cannot parse PEM file: " ++ err
+
+ciphers :: [Cipher]
+ciphers = [
+	cipher_AES128_SHA1,
+	cipher_AES256_SHA1,
+	cipher_RC4_128_MD5,
+	cipher_RC4_128_SHA1
+ ]
--- testWarpTLS.hs
+++ testWarpTLS.hs
@@ -1,31 +1,93 @@
-{-# LANGUAGE PackageImports, OverloadedStrings #-}
+{-# LANGUAGE PackageImports, OverloadedStrings, RankNTypes, KindSignatures #-}
 
+import Params
+
+import Data.Maybe
+import "crypto-random" Crypto.Random
+
+import Data.Conduit
+import Data.Conduit.List as CL
 import Data.Conduit.Network
+import Data.Conduit.Binary
+
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as L
+
+import Network.TLS
 import Network.Wai
-import Network.Wai.Handler.WarpTLS
 import Network.Wai.Handler.Warp
-import Network.Wai.Middleware.RequestLogger (mkRequestLogger)
-import Data.Default (def)
 import Control.Exception
-import Network
+import Control.Monad
+import Control.Applicative
 import "monads-tf" Control.Monad.Trans
 
-import Control.Concurrent
 import Network.HTTP.Types.Status
+import Network.Socket
 
-import Blaze.ByteString.Builder (copyByteString)
+import Data.IORef
 
-tlss = tlsSettings
-	"/home/tatsuya/ssl/csr/2013crt_cross_owl_skami2.pem"
-	"/home/tatsuya/ssl/csr/mydomain.key"
---	"/home/tatsuya/ssl/csr/2013key_owl_skami2.pem"
+crt, key :: String
+crt = "/home/tatsuya/ssl/csr/2013crt_cross_owl_skami2.pem"
+key = "/home/tatsuya/ssl/csr/mydomain.key"
 
 main :: IO ()
 main = do
-	bracket (bindPort 3000 HostAny) sClose $ \socket -> do
---		app <- mkRequestLogger def
-		runTLSSocket tlss defaultSettings socket app
+	bracket (bindPort 3000 HostAny) sClose $ \sock -> do
+		params <- getParams crt key
+		runSettingsConnection defaultSettings (getter params sock) app
+--		runTLSSocket tlss defaultSettings sock app
 		
+app :: MonadTrans t => a -> t IO Response
 app _ = lift $ do
-	print "hello"
+	print ("hello" :: String)
 	return $ responseLBS status200 [("Content-Type", "text/plain")] "PONG"
+
+getter :: Params -> Socket -> IO (Connection, SockAddr)
+getter params sock = do
+	(s, sa) <- accept sock
+	handle (retry s params sock) $ do
+		(fromClient, firstBS) <- sourceSocket s $$+ peek
+		let toClient = sinkSocket s
+		ifromClient <- newIORef fromClient
+		let getNext sink = do
+			fromClient' <- readIORef ifromClient
+			(fromClient'', bs) <- fromClient' $$++ sink
+			writeIORef ifromClient fromClient''
+			return bs
+		if maybe False ((== 0x16) . fst) (firstBS >>= B.uncons)
+		then do	gen <- cprgCreate <$> createEntropyPool
+			ctx <- contextNew Backend {
+				backendFlush = return (),
+				backendClose = return (),
+				backendSend = \bs -> yield bs $$ toClient,
+				backendRecv = getNext . takeMost
+			 } params (gen :: SystemRNG)
+			handshake ctx
+			let conn = Connection {
+				connSendMany = sendData ctx . L.fromChunks,
+				connSendAll = sendData ctx . L.fromChunks . return,
+				connSendFile = \fp offset len _th headers _cleaner -> do
+					sendData ctx $ L.fromChunks headers
+					runResourceT $ sourceFileRange fp (Just offset) (Just len) $$ CL.mapM_ (sendData ctx . L.fromChunks . return),
+				connClose = bye ctx >> sClose s,
+				connRecv = recvData ctx
+			 }
+			return (conn, sa)
+		else do	let conn = (socketConnection s) {
+				connRecv = getNext $
+					fmap (fromMaybe B.empty) await
+			 }
+			return (conn, sa)
+
+retry :: Socket -> TLSParams -> Socket -> SomeException ->
+	IO (Connection, SockAddr)
+retry s a b _ = sClose s >> getter a b
+
+takeMost :: forall (m :: * -> *) o . Monad m => Int -> ConduitM B.ByteString o m B.ByteString
+takeMost i = await >>= maybe (return B.empty) go
+	where
+	go bs = do
+		unless (B.null y) $ leftover y
+		return x
+		where
+		(x, y) = B.splitAt i bs
